4c1bebb950f7f425a58506514c6e5096e44110df
Jesse Farmer <jesse@20bits.com>
2012-05-04 14:57:38 -0700

    Add scoring for 0.0.2 release

--- lib/ruby-pong.rb
+++ lib/ruby-pong.rb
@@ -6,6 +6,14 @@ module Pong
     set_defaults do
       set_width  15
       set_height 100
+
+      set_score 0
+    end
+
+    def score!
+      set_score(self.score + 1).tap do |score|
+        puts "%s scored! It now has %d points." % [self.name, score]
+      end
     end
   end
 
